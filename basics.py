3aab2eb20b03139b1eddcf06b8128a8f7f7a5361
piyushgl <59592007+piyushgl@users.noreply.github.com>
2020-02-26 07:20:32 -0500

    added basics

--- basics.py
+++ basics.py
@@ -1,2 +1,17 @@
 import datetime
-print(datetime.datetime.now())
+date = datetime.datetime.now()
+nbr = 10
+text = "Hello"
+print("date is",datetime.datetime.now())
+print(nbr,text)
+
+# simple datatypes, int,float,str
+x=10
+y="10"
+
+print(x+x)
+print(y+y)
+
+# compound datatypes, list
+rain = ['today',11,5.5,[1,2,3]]
+print(rain)
